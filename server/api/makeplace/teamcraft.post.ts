9cc172ec3cd72a67015aaf25d6326b8e7f13f9ef
oxypomme <tom.sublet@proton.me>
2024-07-20 23:48:43 +0200

    feat(server): :sparkles: enrich items from XIVAPI

--- server/api/makeplace/teamcraft.post.ts
+++ server/api/makeplace/teamcraft.post.ts
@@ -1,4 +1,5 @@
 import { MakePlaceSchemaValidation, parseSchema } from '~/server/lib/makeplace';
+import { enrichItems } from '~/server/lib/xivapi';
 import { getImportLink } from '~/server/lib/teamcraft';
 
 export default defineEventHandler(async (event) => {
@@ -6,16 +7,29 @@ export default defineEventHandler(async (event) => {
     event,
     (body) => MakePlaceSchemaValidation.safeParse(body),
   );
+  // eslint-disable-next-line @typescript-eslint/no-throw-literal
+  if (!result.success) { throw result.error.issues; }
 
-  if (!result.success) {
-    // eslint-disable-next-line @typescript-eslint/no-throw-literal
-    throw result.error.issues;
+  // Parse makeplace file
+  const { items, dyes } = parseSchema(result.data);
+
+  // Enrich items
+  const dyesIds = new Set(dyes.map(({ item }) => item.id));
+  const enriched = await enrichItems([...items, ...dyes]);
+  const enrichedDyes = [];
+  const enrichedItems = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const item of enriched) {
+    if (dyesIds.has(item.item.id)) {
+      enrichedDyes.push(item);
+    } else {
+      enrichedItems.push(item);
+    }
   }
 
-  const { items, dyes } = parseSchema(result.data);
   return {
-    items,
-    dyes,
+    dyes: enrichedDyes,
+    items: enrichedItems,
     link: getImportLink([...items, ...dyes]),
   };
 });
