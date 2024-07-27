9f6e10730c0a0a93edf3d819111b695ad4d58f1d
oxypomme <tom.sublet@proton.me>
2024-07-27 23:00:32 +0200

    feat(server): :loud_sound: added logs on requests and in routes

--- server/api/makeplace/teamcraft.post.ts
+++ server/api/makeplace/teamcraft.post.ts
@@ -1,21 +1,32 @@
+import { appLogger } from '~/server/lib/logger';
 import { MakePlaceSchemaValidation, parseSchema } from '~/server/lib/makeplace';
 import { getImportLink } from '~/server/lib/teamcraft';
 
 export default defineEventHandler(async (event) => {
-  const result = await readValidatedBody(
-    event,
-    (body) => MakePlaceSchemaValidation.safeParse(body),
-  );
+  try {
+    appLogger.debug({ msg: 'Received MakePlace schema...', size: getHeader(event, 'content-length') });
+    const result = await readValidatedBody(
+      event,
+      (body) => MakePlaceSchemaValidation.safeParse(body),
+    );
 
-  if (!result.success) {
-    // eslint-disable-next-line @typescript-eslint/no-throw-literal
-    throw result.error.issues;
+    if (!result.success) {
+      // eslint-disable-next-line @typescript-eslint/no-throw-literal
+      throw result.error.issues;
+    }
+    appLogger.debug('MakePlace schema is valid !');
+
+    appLogger.debug('Parsing MakePlace schema...');
+    const { items, dyes } = parseSchema(result.data);
+    appLogger.info({ msg: 'MakePlace schema parsed !', items: items.length, dyes: dyes.length });
+
+    return {
+      items,
+      dyes,
+      link: getImportLink([...items, ...dyes]),
+    };
+  } catch (error) {
+    appLogger.error('Error while generating import link', error);
+    throw error;
   }
-
-  const { items, dyes } = parseSchema(result.data);
-  return {
-    items,
-    dyes,
-    link: getImportLink([...items, ...dyes]),
-  };
 });
---
+++ server/lib/logger.ts
@@ -0,0 +1,21 @@
+import pino from 'pino';
+import pretty from 'pino-pretty';
+
+
+function createPino(name: string, format?: string) {
+  return pino(
+    { name },
+    pino.multistream([
+      pretty({
+        messageFormat: format,
+        hideObject: !!format,
+        ignore: `pid,hostname`,
+      }),
+      pino.destination(`server/logs/${name}.log`),
+    ])
+  )
+}
+
+export const appLogger = createPino('app');
+
+export const httpLogger = createPino('http', '"{method} {path}" {status} {elapsed}ms');
